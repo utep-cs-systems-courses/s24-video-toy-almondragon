12b6af7a50a1800f71ebcbe759669b95c5529d2f
almondragon <almondragon@miners.utep.edu>
2024-04-27 14:47:14 -0600

    drawing functions files created

---
+++ project/toy/drawCakes.c
@@ -0,0 +1,5 @@
+#include <msp430.h>
+#include "lcdutils.h"
+#include "lcddraw.h"
+#include "drawCakes.h"
+
---
+++ project/toy/drawCakes.h
@@ -0,0 +1,11 @@
+#ifndef drawCakes_included
+#define drawCakes_included
+
+void drawChocolate(int col, int row);
+void drawVanilla(int col, int row);
+void drawBlueberry(int col, int row);
+void drawStrawberry(int col, int row);
+void drawCandles(int col, int row);
+void updateHeights();
+
+extern int layerLimitFlag;
