215c559a56db926656997b05a264542010202fe9
almondragon <almondragon@miners.utep.edu>
2024-04-27 14:36:57 -0600

    stateMachine files created

---
+++ project/toy/stateMachines.c
@@ -0,0 +1,34 @@
+#include <msp430.h>
+#include "stateMachines.h"
+#include "buzzer.h"
+#include "drawCakes.h"
+
+State current_state = begin;
+
+void state_chocolate(){
+
+}
+
+void state_vanilla(){
+
+}
+
+void state_blueberry(){
+
+}
+
+void state_strawberry(){
+
+}
+
+void state_candles(){
+
+}
+
+void state_begin(){
+
+}
+
+void transition_state(State next_state){
+
+}
