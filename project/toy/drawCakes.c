219b3de7822933cc2dd9e72f870cf5ed8d1f2902
christian dees <christianmdees@mgail.com>
2024-04-28 13:44:17 -0600

    fixed buttons and display

--- project/toy/drawCakes.c
+++ project/toy/drawCakes.c
@@ -5,58 +5,30 @@
 #include "stateMachines.h"
 #include "lcdtypes.h"
 
-int limitFlag = 0;
-u_char currentHeight = screenHeight-20;
+
 
 void drawChocolate(int col, int row){
   fillRectangle(20,currentHeight,screenWidth-40,20,COLOR_BROWN);
-  updateHeights();
 }
 
 void drawVanilla(int col, int row){
   fillRectangle(20,currentHeight,screenWidth-40,20,COLOR_BEIGE);
-  updateHeights();
 }
 
 void drawBlueberry(int col, int row){
-  fillRectangle(20,currentHeight,screenWidth-40,10, COLOR_ROYAL_BLUE);
-  updateHeights();
+    fillRectangle(20,currentHeight,screenWidth-40,20, COLOR_ROYAL_BLUE);
 }
 
 void drawStrawberry(int col, int row){
-
+    fillRectangle(20,currentHeight,screenWidth-40,20,COLOR_RED);
 }
 
 void drawCandles(int col, int row){
-    
+    clearScreen(COLOR_RED);
 }
-
-void updateHeights(){
-  currentHeight-=20;
-  limitFlag+=1;
-  if(limitFlag == 4)
-    transition_state(candles);
-}
-
 
 void update_shapes(){
-  switch (current_state){
-      case chocolate:
-        drawChocolate(0,0);
-        break;
-      case vanilla:
-        drawVanilla(0,0);
-        break;
-      case blueberry:
-        drawBlueberry(0,0);
-        break;
-      case strawberry:
-        drawChocolate(0,0);
-        break;
-      case candles:
-        drawCandles(0,0);
-        break;
-    }
-
+    // moving candles here 
 }
+
 
--- project/toy/stateMachines.c
+++ project/toy/stateMachines.c
@@ -1,26 +1,53 @@
 #include "lcdutils.h"
 #include "lcddraw.h" 
+#include "lcdtypes.h"
 #include <msp430.h>
 #include "stateMachines.h"
 #include "buzzer.h"
 #include "drawCakes.h"
 
 State current_state = begin;
+int limitFlag = 0;
+u_char currentHeight = screenHeight-20;
 
+void updateHeights(){
+  currentHeight-=20;
+  limitFlag+=1;
+  if(limitFlag == 5)
+    transition_state(candles);
+}
+
+char display_once = 1;
 void state_chocolate(){
-  
+    if (display_once){
+        drawChocolate(0,0);
+        updateHeights();
+        display_once = 0;
+    }
 }
 
 void state_vanilla(){
-  
+    if (display_once){
+        drawVanilla(0,0);
+        updateHeights();
+        display_once = 0;
+    }
 }
 
 void state_blueberry(){
-
+    if (display_once){
+        drawBlueberry(0,0);
+        updateHeights();
+        display_once = 0;
+    }
 }
 
 void state_strawberry(){
-
+    if (display_once){
+        drawStrawberry(0,0);
+        updateHeights();
+        display_once = 0;
+    }
 }
 
 void state_candles(){
@@ -32,6 +59,8 @@ void state_begin(){
 }
 
 void transition_state(State next_state){
+    display_once = 1;
     redrawScreen = 1;
     current_state = next_state;
 }
+
